1ad860bd7572ba8e89f356b70d024f550cd9d6af
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-01-20 10:54:16 -0800

    add scripts.js functionality - tentative working version.

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,18 @@ function handleFormSubmission(event) {
   let input = parseInt(document.querySelector("input#number").value)
   let numbers = []
 
-  for(let index = 1; index <= input; index += 1) {
-    numbers.push(" " + index)
+  for(let index = 0; index <= input; index += 1) {
+    let indexString = index.toString();
+    if (indexString.includes(3)) {
+      numbers.push(" Won't you be my neighbor");
+    } else if (indexString.includes(2)) {
+      numbers.push(" Boop!");
+    } else if (indexString.includes(1)) {
+      numbers.push(" Beep!");
+    } else {
+      numbers.push(" " + indexString)
   }
-  
+}
 
   document.getElementById("answer").innerText = numbers;
 }
@@ -17,4 +25,4 @@ function handleFormSubmission(event) {
 window.addEventListener("load", function() {
   const form = document.getElementById("question");
   form.addEventListener("submit", handleFormSubmission);
-});
+}); 
