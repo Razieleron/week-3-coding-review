a5d708d9a94fa111e304117f687fa2f47f8cb1a2
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-01-20 11:13:00 -0800

    reinitialize javascript file.  I misread the instructions, and am now coding in tandem with my TDD designs in file TDD.md

---
+++ TDD.md
@@ -0,0 +1,57 @@
+Describe misterRoboger()
+
+Test:
+Code:
+Expected Output:
+
+Test:
+Code:
+Expected Output:
+
+Test:
+Code:
+Expected Output:
+
+Test:
+Code:
+Expected Output:
+
+Test:
+Code:
+Expected Output:
+
+Test:
+Code:
+Expected Output:
+
+Test:
+Code:
+Expected Output:
+
+Test:
+Code:
+Expected Output:
+
+Test:
+Code:
+Expected Output:
+
+Test:
+Code:
+Expected Output:
+
+Test:
+Code:
+Expected Output:
+
+Test:
+Code:
+Expected Output:
+
+Test:
+Code:
+Expected Output:
+
+Test:
+Code:
+Expected Output:
--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,9 @@
-function handleFormSubmission(event) {
+function misterRoboger(event) {
   event.preventDefault();
 
   let input = parseInt(document.querySelector("input#number").value)
   let numbers = []
 
-  for(let index = 0; index <= input; index += 1) {
-    let indexString = index.toString();
-    if (indexString.includes(3)) {
-      numbers.push(" Won't you be my neighbor");
-    } else if (indexString.includes(2)) {
-      numbers.push(" Boop!");
-    } else if (indexString.includes(1)) {
-      numbers.push(" Beep!");
-    } else {
-      numbers.push(" " + indexString)
-  }
-}
 
   document.getElementById("answer").innerText = numbers;
 }
@@ -24,5 +12,5 @@ function handleFormSubmission(event) {
 
 window.addEventListener("load", function() {
   const form = document.getElementById("question");
-  form.addEventListener("submit", handleFormSubmission);
+  form.addEventListener("submit", misterRoboger);
 }); 
